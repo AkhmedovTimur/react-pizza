dea6325829f0825291c93d8b57e294a2e8964d8d
Timur <timline90@gmail.com>
2022-09-02 13:15:26 +0300

    Верстка + Cart css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,41 +6,36 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
 
-
 function Home() {
-
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-
     fetch('https://630b6045ed18e82516525aee.mockapi.io/utems')
       .then((res) => {
-        return res.json()
+        return res.json();
       })
       .then((json) => {
-        setItems(json)
-        setIsLoading(false)
-      })
-  }, [])
-
+        setItems(json);
+        setIsLoading(false);
+      });
+      window.scrollTo(0, 0);
+  }, []);
 
   return (
-   <>
-    <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map((obj) => (
-                  <PizzaBlock key={obj.id} {...obj} />
-                ))}
-          </div>
-   </>
-  )
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
